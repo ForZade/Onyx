246e566847d179f1c77583ebae20be5d4c5618c5
ForZade <lukaslightie7@gmail.com>
2024-02-07 15:02:15 +0200

    Title-bar ipcHandlers added

--- App/src/components/TitleBar.vue
+++ App/src/components/TitleBar.vue
@@ -4,16 +4,16 @@
         <section id="drag" class="h-full grow order-2"></section>
 
         <section class="h-full flex order-3">
-            <div id="minimize" class="winBtn grid h-full cursor-pointer place-items-center w-10 dark:hover:bg-od-hover-1 hover:bg-ol-hover-1 dark:text-od-icon text-ol-icon dark:hover:text-white hover:text-black">
+            <div id="minimize" class="winBtn grid h-full cursor-pointer place-items-center w-10 dark:hover:bg-od-hover-1 hover:bg-ol-hover-1 dark:text-od-icon text-ol-icon dark:hover:text-white hover:text-black" @click="minimize">
                 <Icon icon="tabler:minus" />
             </div>
 
-            <div id="maximize" class="winBtn grid h-full cursor-pointer place-items-center w-10 dark:hover:bg-od-hover-1 hover:bg-ol-hover-1 dark:text-od-icon text-ol-icon dark:hover:text-white hover:text-black">
+            <div id="maximize" class="winBtn grid h-full cursor-pointer place-items-center w-10 dark:hover:bg-od-hover-1 hover:bg-ol-hover-1 dark:text-od-icon text-ol-icon dark:hover:text-white hover:text-black" @click="changeSize">
                 <Icon v-if="isMaximized" icon="tabler:minimize" />
                 <Icon v-else icon="tabler:maximize" />
             </div>
 
-            <div id="close" class="winBtn grid h-full cursor-pointer place-items-center w-10 dark:hover:bg-red-500 dark:text-od-icon text-ol-icon dark:hover:text-white hover:text-black">
+            <div id="close" class="winBtn grid h-full cursor-pointer place-items-center w-10 dark:hover:bg-red-500 dark:text-od-icon text-ol-icon dark:hover:text-white hover:text-black" @click="close">
                 <Icon icon="tabler:x" />
             </div>
         </section>
@@ -22,6 +22,8 @@
 
 <script lang="ts">
 import { Icon } from '@iconify/vue';
+
+const ipcRenderer = window.require('electron').ipcRenderer;
 
 export default {
     name: 'TitleBar',
@@ -32,7 +34,36 @@ export default {
         return {
             isMaximized: false,
         }
-    }
+    },
+    methods: {
+        minimize() {
+            ipcRenderer.send('minimize-window');
+        },
+        changeSize() {
+            ipcRenderer.send('change-window-size');
+        },
+        close() {
+            ipcRenderer.send('close-window');
+        },
+
+        updateWindowState() {
+            ipcRenderer.send('get-window-state');
+            ipcRenderer.on('window-state', (_, isMaximized) => {
+                this.isMaximized = isMaximized;
+            });
+        },
+    },
+    mounted() {
+    // Checks for window maximized state
+    this.updateWindowState();
+
+    // Listen for the resize event to update the window state
+    window.addEventListener('resize', this.updateWindowState);
+  },
+  beforeDestroy() {
+    // Remove the resize event listener when the component is destroyed
+    window.removeEventListener('resize', this.updateWindowState);
+  },
 }
 </script>
 
